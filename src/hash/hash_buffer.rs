46c6b0f069185f5489d88c363d769d463f99e7b6
Sean Allesina-McGrory <allesis@wwu.edu>
2026-02-16 10:35:31 -0800

    Add `ensure_sizing` macro to ensure correct bounds
    We want to make sure we never use a size which is larger than another in
    some places. To do this we can use this macro, ensuring at compiling
    time, we never use bad sizes.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- src/hash/hash_buffer.rs
+++ src/hash/hash_buffer.rs
@@ -72,6 +72,28 @@ macro_rules! commit {
   }};
 }
 
+// Ensure that `$test_size` is less than `$max_size`
+// We could also turn this into a `const fn` as follows:
+//
+// ```
+// const fn ensure_sizing_fn(test_size: usize, max_size: usize) {
+//   assert!(test_size <= max_size);
+// }
+// ```
+//
+// However, this means that we don't get LSP warnings from static analysis
+// So we use a macro to ensure we do
+macro_rules! ensure_sizing {
+  ($test_size:expr, $max_size:expr) => {{
+    // This will allow us to catch bad sizing match ups before runtime
+    // We need the `const _: () =` here or the assert! is not evaluated
+    // until runtime
+    // Which is not what we want
+    const _: () = assert!($test_size as usize <= $max_size as usize);
+  }};
+}
+
 pub(crate) use commit;
+pub(crate) use ensure_sizing;
 pub(crate) use optionize_buffer;
 pub(crate) use rollback;
